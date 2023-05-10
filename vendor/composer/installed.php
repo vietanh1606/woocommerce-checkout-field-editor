385974dac7b7c64980eac6d77cefc566287ef01a
Anh Ngo <anh@wisecommerce.io>
2023-05-10 16:40:58 +0700

    implement

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'woocommerce/woocommerce-checkout-field-editor',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => '94df4c579883bcf7415d03364f2b18012f50c71c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'woocommerce/woocommerce-checkout-field-editor' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => '94df4c579883bcf7415d03364f2b18012f50c71c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
